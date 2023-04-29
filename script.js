58147bf256910fbbc04ec79855d8add2ae85561a
FTTG <32749931+FTTG@users.noreply.github.com>
2023-04-29 13:52:33 -0600

    Allow screen to be populated by operands

--- script.js
+++ script.js
@@ -1,33 +1,62 @@
 const numbers = document.querySelectorAll('.number-key');
 const topDisplay = document.querySelector('#top-display');
 const botDisplay = document.querySelector('#bottom-display');
-const operator = document.querySelectorAll('.operator');
+const operators = document.querySelectorAll('.operator');
+const clearButton = document.querySelector('#clear');
+const equal = document.querySelector('#equal');
+
+const operatorArray = ['+', '-', 'x', '&#247;'];
+
+let num1;
+let num2;
 
 for (let number of numbers) number.addEventListener('click', addToDisplay);
 
+for (let operator of operators) operator.addEventListener('click', checkForOperand);
+
+clearButton.addEventListener('click', clearScreen);
+equal.addEventListener('click', operate);
 
 function addToDisplay() {
-    if (botDisplay.textContent == '0') {
-        botDisplay.textContent = this.textContent;
+    console.log(this);
+    if (topDisplay.textContent == '0') {
+        topDisplay.textContent = this.textContent;
         return;
     }
-    console.log(botDisplay.textContent);
-    console.log(this.textContent);
-    botDisplay.textContent = botDisplay.textContent + this.textContent;
+    topDisplay.textContent = topDisplay.textContent + this.textContent;
 }
 
-function add(num1, num2) {
-    return num1 + num2;
+function checkForOperand() {
+    let result;
+    console.log(this);
+    if (operatorArray.some(inc => topDisplay.textContent.includes(inc))) {
+        result = operate();
+    }
+    addToDisplay.call(this);
 }
 
-function subtract(num1, num2) {
-    return num1 - num2;
+function operate(n1, n2, symbol) {
+    console.log('working bby');
+
 }
 
-function divide(num1, num2) {
-    return num1 / num2;
+function add(n1, n2) {
+    return n1 + n2;
 }
 
-function multiply(num1, num2) {
-    return num1 * num2;
+function subtract(n1, n2) {
+    return n1 - n2;
+}
+
+function divide(n1, n2) {
+    return n1 / n2;
+}
+
+function multiply(n1, n2) {
+    return n1 * n2;
+}
+
+function clearScreen() {
+    topDisplay.textContent = '';
+    botDisplay.textContent = '0';
 }
